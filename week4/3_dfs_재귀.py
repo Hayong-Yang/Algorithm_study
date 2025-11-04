07fffbcb632cd64db3d2ef01841fafd267ae5f06
Hayong Yang <ssk04022@gmail.com>
2025-11-04 16:19:31 +0900

    재귀 dfs

---
+++ week4/3_dfs_재귀.py
@@ -0,0 +1,30 @@
+"""
+Q. 인접 리스트가 주어질 때, 모든 노드를 DFS 순서대로 방문하시오. 
+"""
+
+# 위의 그래프를 예시로 삼아서 인접 리스트 방식으로 표현했습니다!
+graph = {
+    1: [2, 5, 9],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3],
+    5: [1, 6, 8],
+    6: [5, 7],
+    7: [6],
+    8: [5],
+    9: [1, 10],
+    10: [9]
+}
+visited = [] # 1, 2, 3, 4, 5
+
+def dfs_recursion(adjacent_graph, cur_node, visited_array):
+    visited_array.append(cur_node)
+    candidate_list = adjacent_graph.get(cur_node, None)
+    for candidate in candidate_list:
+        if candidate in visited_array:
+            continue
+        dfs_recursion(adjacent_graph, candidate, visited_array)
+    return
+
+dfs_recursion(graph, 1, visited)  # 1 이 시작노드입니다!
+print(visited)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 이 출력되어야 합니다!
