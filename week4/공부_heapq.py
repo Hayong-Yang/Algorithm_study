49507867f432f3bc79f7a7dcea938b443d936f9e
Hayong Yang <ssk04022@gmail.com>
2025-11-06 13:48:13 +0900

    bfs

---
+++ week4/공부_heapq.py
@@ -0,0 +1,33 @@
+import heapq
+
+# heap =[]
+
+# heapq.heappush(heap, 3)
+# heapq.heappush(heap, 5)
+# heapq.heappush(heap, 8)
+
+# print(heap)
+
+# -------------------------------
+
+# array = [3,1,5,6,8,0,3]
+
+# heapq.heapify(array)
+
+# print(array)
+
+# -------------------------------
+
+# heap = [3,1,5,6,8,0]
+# heapq.heapify(heap)
+
+# print(heap)
+# poped = heapq.heappushpop(heap, 9)
+# print(poped)
+# print(heap)
+
+# poped2 = heapq.heapreplace(heap, 10)
+# print(poped2)
+# print(heap)
+
+# -------------------------------
